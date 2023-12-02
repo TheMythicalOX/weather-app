41724ba6760d2df1dcf1478d61805d76a2786625
Elijah <keokline2015@gmail.com>
2023-12-02 00:21:54 -0600

    Tmp

--- src/WeatherBox.js
+++ src/WeatherBox.js
@@ -3,32 +3,65 @@ import { useState } from "react";
 const WeatherBox = () => {
   const [temp, setTemp] = useState("Temp");
   const [wind, setWind] = useState("Wind");
-  const [units, setUnits] = useState("metric");
+  const [units, setUnits] = useState("imperial");
   const [name, setName] = useState("");
+  const [sign, setSign] = useState("F");
+
+  const apiKey = JSON.stringify(process.env.REACT_APP_API_KEY);
 
   const api = {
-    key: "c37881d80ee2eb0518418590500f0104",
+    key: apiKey,
     base: "https://api.openweathermap.org/data/2.5/",
+  };
+
+  const getWeatherData = (unit) => {
+    fetch(`${api.base}weather?q=${name}&units=${unit}&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setTemp(result.main.temp);
+        setWind(result.wind.speed);
+      });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    fetch(`${api.base}weather?q=${name}&units=${units}&APPID=${api.key}`)
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result);
-      });
+    getWeatherData(units);
+    handleSign(units);
+  };
+
+  const handleUnits = (e) => {
+    // console.log(e.target.value);
+    getWeatherData(e.target.value);
+    setUnits(e.target.value);
+    handleSign(e.target.value);
+  };
+
+  const handleSign = (sign) => {
+    if (sign === "imperial") setSign("F");
+    if (sign === "metric") setSign("C");
+    if (sign === "kelvin") setSign("K");
   };
 
   return (
     <div className="weather-box">
-      <h1>{temp}</h1>
-      <h2>{wind}</h2>
+      <h1>
+        Temp: {temp}°{sign}
+      </h1>
+      {sign !== "F" && <h2>Wind: {wind} Km/h</h2>}
+      {sign === "F" && <h2>Wind: {wind} mph</h2>}
       <form>
         <label>Name</label>
         <textarea onChange={(e) => setName(e.target.value)}>{name}</textarea>
         <button onClick={handleSubmit}>Search...</button>
       </form>
+      {temp !== "Temp" && (
+        <select onChange={handleUnits}>
+          <option value="imperial">imperial</option>
+          <option value="metric">metric</option>
+          <option value="kelvin">kelvin</option>
+        </select>
+      )}
     </div>
   );
 };

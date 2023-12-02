fff7f7aee56e22b70612b01214b3aee6b5da3019
Elijah <keokline2015@gmail.com>
2023-12-02 10:01:58 -0600

    basic css and a new bug yay

--- src/WeatherBox.js
+++ src/WeatherBox.js
@@ -3,9 +3,8 @@ import { useState } from "react";
 const WeatherBox = () => {
   const [temp, setTemp] = useState("Temp");
   const [wind, setWind] = useState("Wind");
-  const [baseTemp, setBaseTemp] = useState(0);
-  const [baseWind, setBaseWind] = useState(0);
-  const [name, setName] = useState("");
+  const base = { temp: "", wind: "" };
+  const [search, setSearch] = useState("");
   const [sign, setSign] = useState("F");
 
   const api = {
@@ -13,15 +12,36 @@ const WeatherBox = () => {
     base: "https://api.openweathermap.org/data/2.5/",
   };
 
+  const changeInfo = (i) => {
+    setSign(i);
+    // console.log(i, temp, wind);
+    if (i === "F") {
+      setTemp(Math.round(base.temp));
+      setWind(Math.round(base.wind));
+    }
+    if (i === "C") {
+      setTemp(Math.round(((base.temp - 32) * 5) / 9));
+      setWind(Math.round(base.wind * 1.609));
+    }
+    if (i === "K") {
+      setTemp(Math.round(((base.temp - 32) * 5) / 9 + 273.15));
+      setWind(Math.round(base.wind * 1.609));
+    }
+  };
+
+  const handleUnits = (e) => {
+    // console.log(e.target.value);
+    changeInfo(e.target.value);
+  };
+
   const getWeatherData = () => {
-    fetch(`${api.base}weather?q=${name}&units=imperial&APPID=${api.key}`)
+    fetch(`${api.base}weather?q=${search}&units=imperial&APPID=${api.key}`)
       .then((res) => res.json())
       .then((result) => {
-        // console.log(result);
-        setWind(Math.round(result.wind.speed));
-        setTemp(Math.round(result.main.temp));
-        setBaseTemp(result.main.temp);
-        setBaseWind(result.wind.speed);
+        setTemp(result.main.temp);
+        setWind(result.wind.speed);
+        console.log(temp, wind);
+        changeInfo(sign);
       });
   };
 
@@ -30,27 +50,9 @@ const WeatherBox = () => {
     getWeatherData();
   };
 
-  const handleUnits = (e) => {
-    // console.log(e.target.value);
-    if (e.target.value === "imperial") {
-      setSign("F");
-      setTemp(Math.round(baseTemp));
-      setWind(Math.round(baseWind));
-    }
-    if (e.target.value === "metric") {
-      setSign("C");
-      setTemp(Math.round(((baseTemp - 32) * 5) / 9));
-      setWind(Math.round(baseWind * 1.609));
-    }
-    if (e.target.value === "kelvin") {
-      setSign("K");
-      setTemp(Math.round(((baseTemp - 32) * 5) / 9 + 273.15));
-      setWind(Math.round(baseWind * 1.609));
-    }
-  };
-
   return (
     <div className="weather-box">
+      <div className="padder"></div>
       {temp !== "Temp" && (
         <h1>
           Temp: {temp}°{sign}
@@ -59,18 +61,18 @@ const WeatherBox = () => {
       {temp !== "Temp" && sign !== "F" && <h2>Wind: {wind} Km/h</h2>}
       {temp !== "Temp" && sign === "F" && <h2>Wind: {wind} mph</h2>}
       <form>
-        <label>Name</label>
         <textarea
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter Location..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         ></textarea>
         <button onClick={handleSubmit}>Search...</button>
       </form>
-      {baseTemp !== 0 && (
+      {temp !== "Temp" && (
         <select onChange={handleUnits}>
-          <option value="imperial">imperial</option>
-          <option value="metric">metric</option>
-          <option value="kelvin">kelvin</option>
+          <option value="F">imperial</option>
+          <option value="C">metric</option>
+          <option value="K">kelvin</option>
         </select>
       )}
     </div>
